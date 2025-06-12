c5505a643810380ba1ecbef460ddb35917ddea9c
smartflomeet <atul.titare@tatatel.co.in>
2025-06-12 14:32:09 +0530

    initial release

---
+++ Package.swift
@@ -0,0 +1,20 @@
+
+// swift-tools-version:5.7
+import PackageDescription
+
+let package = Package(
+    name: "EnablexWebRTC",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(name: "EnablexWebRTC", targets: ["EnablexWebRTC"])
+    ],
+    targets: [
+        .binaryTarget(
+            name: "EnablexWebRTC",
+            url: "https://developer.enablex.io/downloads/EnablexWebRTC.xcframework_1.0.4.zip",
+            checksum: "6ef4e8c6f78eddbc9d4f478ea14fb111cf58199a976b15aff37779ae0fe04b6c"
+        )
+    ]
+)
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # EnablexWebRTC
-This is the SPM for EnablexWebRTC file instalation
+
